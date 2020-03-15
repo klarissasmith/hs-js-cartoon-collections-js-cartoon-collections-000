af8d09e74c4ae11095aa3c7636b46c1e490b10cc
Klarissa Smith <ksmith_email@yahoo.com>
2020-03-15 03:46:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
+  var newdwarves = [];
+  for (let i = 0; i < dwarves.length; i++){
+    newdwarves.push(`${i + 1}. ${dwarves[i]} `)
+    }
+return newdwarves.join("")
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var newplaneteerCalls = []
+  for (let i = 0; i < planeteerCalls.length; i++){
+    newplaneteerCalls.push(`${planeteerCalls[i].toUpperCase()}!`)
+    }
+return newplaneteerCalls
 }
 
 function longPlaneteerCalls(words) {
-}
+  for (let i = 0; words[i].toString().length > 4; i++){
+    return true
+    }
+    return false
+} 
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++){
+    if (foods.includes("cheddar")){
+    return "cheddar"
+    }
+return "no cheese!"
+}
 }
